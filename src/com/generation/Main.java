c27da181f9d5f353349b1b18ce0cb409fab9507d
Sheila Sanchez Ramirez <sanchezramirezsheila@gmail.com>
2021-10-11 17:40:42 -0500

    Introducción al tipo de datos primitivos de Java y su conversión

---
+++ src/com/generation/Main.java
@@ -0,0 +1,79 @@
+package com.generation;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// const nombre='Sheila';
+        // Variable String
+//        String nombre="Sheila";
+
+        //datos primitivos numéricos
+//        byte num1=10;
+//        short num2=10;
+//        int num3=10;
+//        long num4=10;
+
+        //datos primitivos decimales. Los números decimales float deben llevar una f al final
+//        float decimal1=10.1212323f;
+//        //datos primitivos decimales. Los números decimales double deben llevar una d al final
+//        double decimal2=20.011120d;
+//
+//
+//        //Dato primitivo char. Debe ir en comilla simple
+//        char caracter='f';
+//
+//        //Dato boolean
+//        boolean verdadero=true;
+//
+//        //Se cambia de manera automática
+//        //byte->short->int->long->float->double
+//        short numShort1=num1;
+//        int numInt1=numShort1;
+//        long numLong1=numInt1;
+//        //etc. La operación inversa no es posible.
+//
+//        //Para la conversión inversa: (existe la posibilidad de pérdida de información)
+//        short numShort2=(short) numLong1;
+//
+//        //ejercicio suma
+//        byte numByte1=120;
+//        byte numByte2=120;
+//        //se declara como short por el rango de valores que admite byte. Hay que hacer la conversión
+//        short suma=(short)(numByte1+numByte2);
+//
+////        System.out.println(suma);
+////        System.out.println(numShort2);
+//
+//        // Operadores aritméticos
+//        // + sumar o concatenar cadenas
+//        // - restar
+//        // * multiplicar
+//        // / dividir
+//        // % residuo
+//
+        //////////Mis operaciones/////////
+//
+//        byte numA=3;
+//        byte numB=12;
+//
+//        byte suma1=(byte) (numA+numB);
+//        int resta= numA-numB;
+//        int producto=numA*numB;
+//        int division=numB/numA;
+//
+//        int numC=6;
+//        float numD=140;
+//
+//        double div=numD/numC;
+//
+//
+//
+//        System.out.println(suma1);
+//        System.out.println(resta);
+//        System.out.println(producto);
+//        System.out.println(division);
+//        System.out.println(div);
+
+
+    }
+}
